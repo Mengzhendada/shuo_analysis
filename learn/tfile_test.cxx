9eabdb3a27233420cadecb53c1a5f7b566023b0b
Shuo <shuojia@jlab.org>
2019-09-12 11:59:20 -0400

    modified:   learn/tfile_test.cxx

--- learn/tfile_test.cxx
+++ learn/tfile_test.cxx
@@ -1,16 +1,18 @@
 #include "TH1.h"
 #include "TFile.h"
 #include "TCanvas.h"
+#include "TH1F.h"
 
 void tfile_test(){
-TH1* h1 = new TH1F("1","11",100,0.0,4);
+TH1* h1 = new TH1D("1","11",100,0,1);
 h1->FillRandom("gaus",1000);
-TH1* h2 = new TF1F("f","ff",100,-3,3);
-
-for(int i = 0;i<10000;i++){
-  h2->Fill(h1->GetRandom());
+TH1* h2 = new TH1D("f","ff",100,-3,3);
+h2->SetCanExtend(TH1::kAllAxes);
+double a[20] = {1,2,3,4,5,6,7,8,9,5,5,5,5,5,5,4,4,4,6,6};
+for(int i = 0;i<20;i++){
+  h2->Fill(a[i]);
 }
-TCanvas *c = new TCanvas();
+TCanvas *c1 = new TCanvas();
 c1->Divide(1,2);
 c1->cd(1);
 h1->Draw();
