d6bfda40c1ad724d9d802fd8d16e5c4737c0bc43
shuojia <shuojia@temple.edu>
2019-10-25 17:35:27 -0400

    new file:   calorimeter_gain_analysis/pcal_calib_7120.calib 	modified:   learn/readrawroot.cxx

--- learn/readrawroot.cxx
+++ learn/readrawroot.cxx
@@ -2,13 +2,18 @@
 #include <iostream>
 #include "TCanvas.h"
 int readrawroot(){
-  std::string rootfile = std::string("ROOTfiles/coin_replay_production_6111_6111.root");
+  std::string rootfile = std::string("ROOTfiles/coin_replay_production_7647_7647.root");
   ROOT::RDataFrame d("T",rootfile);
-  auto h_P_etottracknorm = d.Histo1D({"shmscal","etottracknorm",100,0.1,2},"P.cal.etottracknorm");
+  //d.Define("P_cal_showertracknorm",[](double tot, double pr){return tot-pr;},{"P.cal.etottracknorm","P.cal.eprtracknorm"});
+  auto h_1 = d.Histo1D({"","shms theta",100,-0.1,0.1},"P.gtr.th");
+  //auto h_2 = d.Histo2D({"","",100,0.01,2,100,0.01,2},"P.cal.eprtracknorm","P_cal_showertracknorm");
   //d.Histo1D({"hmsgtrdp","hms goodtr dp",100,-14,14},"H.gtr.dp");
   std::cout<<"nentries "<<*d.Count()<<std::endl;
-  auto *c = new TCanvas;
-  h_P_etottracknorm->DrawCopy();
+  auto *c = new TCanvas();
+  h_1->DrawCopy();
   c->SaveAs("results/readrawroot.pdf");
+  //auto *c2 = new TCanvas();
+  //h_2->DrawCopy();
+  //c2->SaveAs("results/readrawroot2.pdf");
   return 0;
 }
