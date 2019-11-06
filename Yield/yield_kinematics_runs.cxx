9345f41c4413131a82b4217229edecdc732bc983
Shuo Jia <sjia@beboplogin1.lcrc.anl.gov>
2019-11-06 15:56:44 -0600

    new file:   Yield/yield_kinematics_runs.cxx 	modified:   Yield/yield_ratio.cxx 	renamed:    Yield/yield_ratio.cxx -> Yield/yield_ratio_old.cxx

---
+++ Yield/yield_kinematics_runs.cxx
@@ -0,0 +1,120 @@
+#include "ROOT/RDataFrame.hxx"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TFile.h"
+#include "Math/Vector3D.h"
+#include "Math/Vector4D.h"
+#include "Math/VectorUtil.h"
+#include "ROOT/RVec.hxx"
+#include "TVector3.h"
+
+#include "nlohmann/json.hpp"
+using json = nlohmann::json;
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+constexpr const double M_P     = 0.938272;
+constexpr const double M_P2    = M_P * M_P;
+constexpr const double M_pion  = 0.139;
+constexpr const double M_pion2 = M_pion * M_pion;
+constexpr const double M_e     = 0.000511;
+//double Eb = 10.6;
+using Pvec3D = ROOT::Math::XYZVector;
+using Pvec4D = ROOT::Math::PxPyPzMVector;
+
+auto p_proton = [](double px, double py, double pz){
+  return Pvec4D{px , py , pz , M_P};
+};
+auto p_pion = [](double px, double py, double pz) {
+  return Pvec4D{px , py , pz , M_pion};
+};
+auto pion_momentum = [](double px,double py,double pz){
+  TVector3 v(px,py,pz);
+  return v;
+};
+auto p_electron = [](double px, double py, double pz) {
+  return Pvec4D{px , py , pz , M_e};
+};
+auto p_q = [](Pvec4D& pe ) {
+  return Pvec4D{0.0,0.0,10.6, M_e}-pe;
+};
+auto t = [](const double Egamma, Pvec4D& jpsi) {
+  Pvec4D beam{0, 0, Egamma, 0};
+  return (beam - jpsi).M2();
+};
+auto z = [](Pvec4D& pq, Pvec4D& ph) {
+  return ph.E()/pq.E();
+};
+auto xbj = [](double Q2,Pvec4D& pq) {
+  return Q2/(2.0*0.938*pq.E());
+};
+auto Q2 = [](Pvec4D& pq) {
+  return -1.0*(pq.Dot(pq));
+};
+auto Wprime2 = [](Pvec4D& pq,Pvec4D& ph) {
+  auto Ptot = Pvec4D{0.0,0.0,0.0, M_P} + pq - ph;
+  return Ptot.Dot(Ptot);
+};
+auto W2 = [](Pvec4D& pq) {
+  auto Ptot = Pvec4D{0.0,0.0,0.0, M_P} + pq;
+  return Ptot.Dot(Ptot);
+};
+
+std::string goodTrackSHMS = "P.gtr.dp>-10 && P.gtr.dp<22";
+std::string goodTrackHMS = "H.gtr.dp>-10 && H.gtr.dp<10";
+
+std::string piCutSHMS = "P.aero.npeSum >1.0 && P.cal.eprtracknorm <0.2 && P.cal.etottracknorm<1.0";
+std::string eCutHMS = "H.cal.etottracknorm >0.8 && H.cal.etottracknorm <2. && "
+                       "H.cer.npeSum > 1.";
+std::string epiCut = "P.aero.npeSum > 1.0 && P.cal.eprtracknorm <0.2 && "
+                     "H.cer.npeSum > 1.0 && H.cal.etottracknorm > 0.6 && "
+                     "H.cal.etottracknorm < 2.0 && P.cal.etottracknorm <1.0";
+
+void yield_kinematics_runs(int RunNumber=0){
+  
+  if(RunNumber ==0){
+    std::cout<<"Enter a RunNumber (-1 to exit):";
+    std::cin>>RunNumber;
+    if(RunNumber<=0)
+      return;
+  }
+ 
+  std::string rootfile_name = "ROOTfiles/coin_replay_production_"+std::to_string(RunNumber)+"_"+std::to_string(RunNumber)+".root";
+  ROOT::RDataFrame d("T",rootfile_name);
+  ROOT::RDataFrame d_scale("TSP",rootfile_name);
+  double charge = *d_scale.Max("P.BCM4B.scalerChargeCut");
+  std::cout<<charge<<std::endl;
+
+  auto d_coin = d.Filter("fEvtHdr.fEvtType == 4");
+  auto d_CoinGoodTrack = d_coin.Filter(goodTrackHMS)
+  .Filter(goodTrackSHMS)
+  ;
+  auto d_Coin_sidis = d_CoinGoodTrack.Filter(epiCut)
+      .Define("p_electron", p_electron, {"H.gtr.px", "H.gtr.py", "H.gtr.pz"})
+      .Define("p_proton",p_proton, {"P.gtr.px", "P.gtr.py", "P.gtr.pz"})
+      .Define("p_pion", p_pion, {"P.gtr.py", "P.gtr.px", "P.gtr.pz"})
+      .Define("p_pion_HMS", p_pion, {"H.gtr.px", "H.gtr.py", "H.gtr.pz"})
+      .Define("p_q", p_q, {"p_electron"})
+      .Define("z", z, {"p_q","p_pion"})
+      .Define("Q2", Q2, {"p_q"})
+      .Define("xbj", xbj, {"Q2","p_q"})
+      .Define("W2",W2,{"p_q"})
+      .Define("Wp2",Wprime2,{"p_q","p_pion"})
+      .Define("W","std::sqrt(W2)")
+      .Define("Wp","std::sqrt(Wp2)")
+      ;
+    
+  std::string rootfile_out_name = "results/yield/kinematics_yield_"+std::to_string(RunNumber)+".root";
+  TFile *rootfile_out = new TFile(rootfile_out_name.c_str(),"RECREATE");
+  auto h_xbj = d_Coin_sidis.Histo1D({"xbj","xbj;xbj;yield",100,0,1},"xbj");
+  h_xbj->Scale(1/charge);
+  h_xbj->Write();
+  auto h_z = d_Coin_sidis.Histo1D({"z","z;z;yield",100,0,1},"z");
+  h_z->Scale(1/charge);
+  h_xbj->Write();
+  rootfile_out->Close();
+}
--- Yield/yield_ratio.cxx
+++ Yield/yield_ratio.cxx
@@ -1,11 +1,10 @@
 #include "ROOT/RDataFrame.hxx"
 #include "TCanvas.h"
 #include "TChain.h"
-#include "TGraphErrors.h"
+#include "TFile.h"
 
 #include "nlohmann/json.hpp"
 using json = nlohmann::json;
-#include "TChain.h"
 
 #include <iostream>
 #include <fstream>
@@ -13,99 +12,20 @@ using json = nlohmann::json;
 #include <sstream>
 #include <vector>
 
-std::string goodTrackSHMS = "P.gtr.dp>-10 && P.gtr.dp<22";
-std::string goodTrackHMS = "H.gtr.dp>-10 && H.gtr.dp<10";
-
-std::string piCutSHMS = "P.aero.npeSum >1.0 && P.cal.eprtracknorm <0.2 && P.cal.etottracknorm<1.0";
-std::string eCutHMS = "H.cal.etottracknorm >0.8 && H.cal.etottracknorm <2. && "
-                       "H.cer.npeSum > 1.";
-std::string epiCut = "P.aero.npeSum > 1.0 && P.cal.eprtracknorm <0.2 && "
-                     "H.cer.npeSum > 1.0 && H.cal.etottracknorm > 0.6 && "
-                     "H.cal.etottracknorm < 2.0 && P.cal.etottracknorm <1.0";
-
 void yield_ratio(){
+
   json j;
   {
     std::ifstream runs("db2/ratio_run_group_updated.json");
     runs>>j;
   }
-  TGraphErrors *G_yield_z = new TGraphErrors();
-  G_yield_z->SetTitle("ratio vs z");
-  TGraphErrors *G_yield_x = new TGraphErrors();
-  G_yield_x->SetTitle("ratio vs x");
-  int i_z=0,i_x=0;
+
   for(json::iterator it = j.begin();it!=j.end();++it){
-    std::vector<int> neg_D2_runs,pos_D2_runs;
+    std::vector<int> neg_D2_runs, pos_D2_runs;
     auto runjs = it.value();
     neg_D2_runs = runjs["neg"]["D2"].get<std::vector<int>>();
     pos_D2_runs = runjs["pos"]["D2"].get<std::vector<int>>();
-    std::cout<<"Rungroup "<<it.key()<<" neg runs "<<neg_D2_runs.size()<<" pos runs "<<pos_D2_runs.size()<<std::endl;
-  if(neg_D2_runs.size()!=0 && pos_D2_runs.size()!=0){
-    TChain chain_neg("T");
-    double neg_D2_charge = 0;
-    for(std::vector<int>::iterator it = neg_D2_runs.begin();it!=neg_D2_runs.end();++it){
-      int RunNumber = *it;
-      std::string rootfile_name = "ROOTfiles/coin_replay_production_"+std::to_string(RunNumber)+"_"+std::to_string(RunNumber)+".root";
-      ROOT::RDataFrame d_sh("TSP",rootfile_name);
-      double charge = *d_sh.Max("P.BCM4B.scalerChargeCut");
-      neg_D2_charge += charge;
-      chain_neg.Add(rootfile_name.c_str());
-      //ROOT::RDataFrame d("T",rootfile_name);
-      //auto dCoinGoodTrack = d.Filter(goodTrackHMS)
-      //  .Filter(goodTrackSHMS)
-      //  .Filter(epiCut)
-      //  ;
-    }//loop over all neg D2 runs
-    ROOT::RDataFrame d_neg(chain_neg);
-    auto dCoinGoodTrack_neg = d_neg.Filter(goodTrackHMS)
-      .Filter(goodTrackSHMS)
-      ;
-    auto dCoinSidis_neg = dCoinGoodTrack_neg.Filter("fEvtHdr.fEvtType ==4 ")
-      .Filter(epiCut)
-      ;
-    double neg_statistics = *dCoinSidis_neg.Count();
-    double yield_neg = neg_statistics/neg_D2_charge;
     
-    TChain chain_pos("T");
-    double pos_D2_charge = 0;
-    for(std::vector<int>::iterator it = pos_D2_runs.begin();it!=pos_D2_runs.end();++it){
-      int RunNumber = *it;
-      std::string rootfile_name = "ROOTfiles/coin_replay_production_"+std::to_string(RunNumber)+"_"+std::to_string(RunNumber)+".root";
-      ROOT::RDataFrame d_sh("TSP",rootfile_name);
-      double charge = *d_sh.Max("P.BCM4B.scalerChargeCut");
-      pos_D2_charge += charge;
-      chain_pos.Add(rootfile_name.c_str());
-      //ROOT::RDataFrame d("T",rootfile_name);
-      //auto dCoinGoodTrack = d.Filter(goodTrackHMS)
-      //  .Filter(goodTrackSHMS)
-      //  .Filter(epiCut)
-      //  ;
-    }//loop over all pos D2 runs
-    ROOT::RDataFrame d_pos(chain_pos);
-    auto dCoinGoodTrack_pos = d_pos.Filter(goodTrackHMS)
-      .Filter(goodTrackSHMS)
-      ;
-    auto dCoinSidis_pos = dCoinGoodTrack_pos.Filter("fEvtHdr.fEvtType ==4 ")
-      .Filter(epiCut)
-      ;
-    double pos_statistics = *dCoinSidis_pos.Count();
-    double yield_pos = pos_statistics/pos_D2_charge;
-    
-    double z = runjs["z"].get<double>();
-    double x = runjs["x"].get<double>();
-    double ratio = yield_neg/yield_pos;
-    G_yield_x ->SetPoint(i_x,x,ratio);
-    G_yield_z ->SetPoint(i_z,z,ratio);
-    ++i_x;
-    ++i_z;
-  }//if pos neg runs exist
-  }//loop over json file
-  TCanvas *c = new TCanvas();
-  c->Divide(1,2);
-  c->cd(1);
-  G_yield_x->Draw();
-  c->cd(2);
-  G_yield_z->Draw();
-  c->SaveAs("results/ratio_x_z.pdf");
-}
+  }
 
+}
