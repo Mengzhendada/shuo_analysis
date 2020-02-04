cb5c2c26a35ca1336aba81d580444171d6584090
Shuo Jia <sjia@beboplogin4.lcrc.anl.gov>
2020-02-04 07:11:20 -0600

    modified:   learn/read_branch_name.cxx

--- learn/read_branch_name.cxx
+++ learn/read_branch_name.cxx
@@ -4,23 +4,29 @@
 #include "TCanvas.h"
 
 void read_branch_name(){
-  std::string root_name = "rootfiles/coin_replay_production_6482_20000.root";
+  std::string root_name = "ROOTfiles/coin_replay_production_7593_7593.root";
   ROOT::RDataFrame d("T",root_name.c_str());
   auto colNames = d.GetColumnNames();
-  std::ofstream of("name.txt");
+  std::ofstream of("results/name.txt");
   for (auto &&colName : colNames) of << colName << std::endl;
   std::cout<<"try"<<std::endl;
   //auto h1 = d.Filter("!P.hod.1x.negAdcPulseAmpRaw.empty()").Define("test","P.hod.1x.negAdcPulseAmpRaw[0]").Histo1D("test");
   //auto h2 = d.Histo1D("P.hod.1x.negAdcPulseAmpRaw");
-   d.Define("test","P.hgcer.npeSum");
-  auto h1 = d.Histo1D("test");
+   auto d1 = d.Define("test","P.hgcer.npe[2]");
+   
+  std::cout<<"try1"<<std::endl;
+   auto h1 = d1.Histo1D({"","",100,0,50},"test");
   //auto h1 = d.Histo1D("P.hgcer.npe[0]");
-  auto h2 = d.Histo1D("P.hgcer.npeSum");
-  TCanvas *c = new TCanvas();
+  std::cout<<"try2"<<std::endl;
+  auto h2 = d.Histo1D({"","",100,0,50},"P.hgcer.npeSum");
+  std::cout<<"try3"<<std::endl;
+  TCanvas *c = new TCanvas("","",1900,1000);
   c->Divide(1,2);
   c->cd(1);
+  std::cout<<"try4"<<std::endl;
   h1->DrawClone();
   c->cd(2);
+  std::cout<<"try5"<<std::endl;
   h2->DrawClone();
-  c->SaveAs("test.pdf");
+  c->SaveAs("results/test.pdf");
 }
