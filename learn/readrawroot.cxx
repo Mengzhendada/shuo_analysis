eead5eed483533a5be97febd4c7b941ce86477c5
shuojia <shuojia@temple.edu>
2020-06-08 15:24:49 -0400

    new file:   all_branch_name.txt 	modified:   learn/readrawroot.cxx

--- learn/readrawroot.cxx
+++ learn/readrawroot.cxx
@@ -5,12 +5,12 @@ int readrawroot(){
   std::string rootfile = std::string("ROOTfiles/coin_replay_production_7647_7647.root");
   ROOT::RDataFrame d("T",rootfile);
   //d.Define("P_cal_showertracknorm",[](double tot, double pr){return tot-pr;},{"P.cal.etottracknorm","P.cal.eprtracknorm"});
-  auto h_1 = d.Histo1D({"","shms theta",100,-0.1,0.1},"P.gtr.th");
-  //auto h_2 = d.Histo2D({"","",100,0.01,2,100,0.01,2},"P.cal.eprtracknorm","P_cal_showertracknorm");
+  //auto h_1 = d.Histo1D({"","shms theta",100,-0.1,0.1},"P.gtr.th");
+  auto h_2 = d.Histo2D({"","",100,-0.8,0.8,100,-80,80},"P.dc.xp_fp","P.dc.x_fp");
   //d.Histo1D({"hmsgtrdp","hms goodtr dp",100,-14,14},"H.gtr.dp");
   std::cout<<"nentries "<<*d.Count()<<std::endl;
   auto *c = new TCanvas();
-  h_1->DrawCopy();
+  h_2->DrawCopy();
   c->SaveAs("results/readrawroot.pdf");
   //auto *c2 = new TCanvas();
   //h_2->DrawCopy();
