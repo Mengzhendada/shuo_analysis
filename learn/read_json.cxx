55aea2eedf5f91300607099a08f42d63e172fd03
Shuo Jia <shuojia@Shuos-MacBook-Air.local>
2021-04-14 23:20:00 -0400

    new file:   latex/csv_yield/make_latex_xbj.cxx 	modified:   learn/read_json.cxx

--- learn/read_json.cxx
+++ learn/read_json.cxx
@@ -1,18 +1,25 @@
 #include "nlohmann/json.hpp"
 #include <string>
 #include <iostream>
-using nlohmann::json;
+using json = nlohmann::json;
 #include <vector>
 #include <fstream>
 
 void read_json(){
 json j;
-{
-  std::ifstream infile("results/ratio_run_group.json");
-  infile>>j;
-}
-std::vector<int> runs;
-runs=j["3"]["neg"]["D2"].get<std::vector<int>>();
-std::cout<<runs[0];
+//{
+//  std::ifstream infile("db2/ratio_run_group_updated.json");
+//  infile>>j;
+//}
+//std::vector<int> runs;
+//runs=j["70"]["neg"]["D2"].get<std::vector<int>>();
+//std::cout<<runs[0];
 //std::cout<<j["1"]["neg"]["D2"]<<std::endl;
+j["1"] = 1;
+//std::cout<<j["1"]<<std::endl;
+std::ofstream ofs;
+ofs.open("try.json");
+//ofs<< j <<std::endl;
+//ofs<<j.dump(4)<<std::endl;
+//ofs<<"hello world"<<std::endl;
 }
